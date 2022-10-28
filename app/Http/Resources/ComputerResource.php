d62686cb274c15ed8f801e79fae3409b630ac785
PatrickBond27 <N00201161@iadt.ie>
2022-10-28 14:43:09 +0100

    implemented API

---
+++ app/Http/Resources/ComputerResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ComputerResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'brand' => $this->brand,
+            'description' => $this->description,
+            'graphics_card' => $this->graphics_card,
+            'processor' => $this->processor,
+        ];
+    }
+}
