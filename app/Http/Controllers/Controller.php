ac37c59bca478add5b3fb598ec8b93a8e81fa955
PatrickBond27 <N00201161@iadt.ie>
2022-11-01 01:50:11 +0000

    Installed Swagger and added APIs

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,21 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+ /**
+ * @OA\Info(
+ *      version="2.0.0",
+ *      title="Computer API",
+ *      description="Patrick Simple Computer API",
+ *      @OA\Contact(
+ *          email="n00201161@iadt.ie"
+ *      ),
+ *     @OA\License(
+ *         name="Apache 2.0",
+ *         url="https://www.apache.org/licenses/LICENSE-2.0.html"
+ *     )
+ * )
+ */
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
